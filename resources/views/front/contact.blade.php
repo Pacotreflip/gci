c2313a5327ec927d7575b31ee114bb681a2bba4e
froke <froke@JFEG>
2016-02-05 10:33:10 -0600

    Formulario de Contact contorno

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -60,33 +60,33 @@
                 <div class="status alert alert-success" style="display: none"></div>
                 {!! Form::open(['url' => 'contact', 'method' => 'post', 'role' => 'form', 'class' => 'contact-form']) !!}	
                         <div class="col-sm-5 col-sm-offset-1">
-                        <div class="form-group">
+                        <div>
                             <label>Nombre *</label>
                             <input type="text" name="name" class="form-control" required="required">
                         </div>
-                        <div class="form-group">
+                        <div>
                             <label>Correo Electrónico *</label>
                             <input type="email" name="email" class="form-control" required="required">
                         </div>
-                        <div class="form-group">
+                        <div>
                             <label>Teléfono</label>
                             <input type="number" class="form-control">
                         </div>
-                        <div class="form-group">
+                        <div>
                             <label>Empresa</label>
                             <input type="text" class="form-control">
                         </div>                        
                     </div>
                     <div class="col-sm-5">
-                        <div class="form-group">
+                        <div>
                             <label>Asunto *</label>
                             <input type="text" name="subject" class="form-control" required="required">
                         </div>
-                        <div class="form-group">
+                        <div>
                             <label>Mensaje *</label>
                             <textarea name="message" id="message" required="required" class="form-control" rows="8"></textarea>
                         </div>                        
-                        <div class="form-group">
+                        <div>
                             <input class="btn btn-primary" type="submit" value="Enviar Mensaje">
                         </div>
                     </div>
