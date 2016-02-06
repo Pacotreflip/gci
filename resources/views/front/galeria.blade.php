1a25809e99aa54b390cd33777d0e1541a7751dd3
froke <froke@JFEG>
2016-02-06 12:13:34 -0600

    Descripción de Imágenes en la galería

--- resources/views/front/galeria.blade.php
+++ resources/views/front/galeria.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="center">
                <hr><h2 class="intro-text text-center">Galería</h2><hr>
-               <p class="lead">descripción</p>
+               <p class="lead">Algúnas de nuestras instalaciones.</p>
             </div>
         
 
@@ -22,8 +22,8 @@
                             {{ HTML::image('images/portfolio/recent/1.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Sistema de Aire Acondicionado</a></h3>
+                                    <p>Sistema de aire de presición para Site de Cómputo.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/1.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -35,8 +35,8 @@
                             {{ HTML::image('images/portfolio/recent/2.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Pasillo Técnico</a></h3>
+                                    <p>Unidades manejadoras de aire en galería técnica.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/2.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -48,8 +48,8 @@
                             {{ HTML::image('images/portfolio/recent/3.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Unidad Manejadora de Aire</a></h3>
+                                    <p>UMA tipo sanitario para areas limpias.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/3.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -61,8 +61,8 @@
                             {{ HTML::image('images/portfolio/recent/4.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Red de ductería</a></h3>
+                                    <p>Sistema de ductería tipo sanitario para areas limpias.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/4.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -74,8 +74,8 @@
                             {{ HTML::image('images/portfolio/recent/5.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Unidades Condensadoras</a></h3>
+                                    <p>Unidades condensadoras para equipos tipo Mini-Split y Fan & Coil para oficinas.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/5.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -87,8 +87,8 @@
                             {{ HTML::image('images/portfolio/recent/6.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Unidad Manejadora de Aire</a></h3>
+                                    <p>UMA Con perfiles de aluminio, paneles de lámina galvanizada y acabado sanitario</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/6.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -100,8 +100,8 @@
                             {{ HTML::image('images/portfolio/recent/7.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Unidad Manejadora de Aire</a></h3>
+                                    <p>UMA Tipo sanitario con perfil de acero galv. acabado en esmalte, para areas limpias.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/7.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
@@ -113,9 +113,35 @@
                             {{ HTML::image('images/portfolio/recent/8.jpg','', array('class'=>'img-responsive')) }}
                             <div class="overlay">
                                 <div class="recent-work-inner">
-                                    <h3><a href="#">Título</a></h3>
-                                    <p>Descripción</p>
+                                    <h3><a href="#">Red de Ductería</a></h3>
+                                    <p>Sistema de ductería en galaría técnica.</p>
                                     <a class="preview" href="{{ asset('images/portfolio/recent/8.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
+                                </div> 
+                            </div>
+                        </div>          
+                    </div><!--/.portfolio-item-->
+                    
+                    <div class="portfolio-item wordpress html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            {{ HTML::image('images/portfolio/recent/9.jpg','', array('class'=>'img-responsive')) }}
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Fan & Coil</a></h3>
+                                    <p>Instalación de Fan & Coil y ductería.</p>
+                                    <a class="preview" href="{{ asset('images/portfolio/recent/9.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
+                                </div> 
+                            </div>
+                        </div>          
+                    </div><!--/.portfolio-item-->
+                    
+                    <div class="portfolio-item wordpress html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            {{ HTML::image('images/portfolio/recent/10.jpg','', array('class'=>'img-responsive')) }}
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Instalación de Ductería</a></h3>
+                                    <p>Ductería para colección de polvos.</p>
+                                    <a class="preview" href="{{ asset('images/portfolio/recent/10.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i> Ver Completa</a>
                                 </div> 
                             </div>
                         </div>          
